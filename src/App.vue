576f163d25165ed176f6ac8988a754d4c82fd64d
JonJaryongLee <director9303@gmail.com>
2024-02-03 11:26:15 +0900

    env changed

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+const TITLE_NAME = import.meta.env.VITE_APP_TITLE_NAME;
 </script>
 
 <template>
   <header class="p-3 bg-dark text-white">
     <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">11기 광주 6반 출석부</a>
+        <a class="navbar-brand" href="#">${ TITLE_NAME }</a>
         <b-navbar-toggle target="navbarNav"></b-navbar-toggle>
         <b-collapse id="navbarNav" is-nav>
           <ul class="navbar-nav">
